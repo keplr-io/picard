ab3e2139451746c89f08ea14443a2483077925a4
jake <jake@keplr.io>
2017-01-26 10:48:20 -0800

    add preparse test

--- picard/parser/parse.py
+++ picard/parser/parse.py
@@ -1,13 +1,16 @@
-from inversion import parse_inversion
-from reduction import parse_repeat
-from optional import parse_optional
-from hp import parse_hp
+from __future__ import absolute_import
+
+from .inversion import parse_inversion
+from .repeat import parse_repeat
+from .optional import parse_optional
+from .hp import parse_hp
 
 def parse(spec):
     return parse_hp(
         preparse(spec)
     )
 
+# parse without adding hyperopt objects
 def preparse(spec):
     return parse_optional(
         parse_repeat(
